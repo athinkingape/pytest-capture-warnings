04acd9665d995d8fcc3c7e01cfebc9c85e1bf5c5
Nandu Pokhrel <nandu.p@athinkingape.com>
2022-03-11 17:03:52 -0500

    pytest capture warnings

--- pytest_capture_deprecatedwarnings/__init__.py
+++ pytest_capture_deprecatedwarnings/__init__.py
@@ -1,17 +1,14 @@
 import os
 import sys
-import json
 import pytest
 import warnings
 import traceback
-
 from _pytest.recwarn import WarningsRecorder
 
 if sys.version_info >= (3, 8):
     from importlib import metadata as importlib_metadata
 else:
     import importlib_metadata
-
 
 counted_warnings = {}
 warnings_recorder = WarningsRecorder()
@@ -93,9 +90,6 @@ def pytest_runtest_call(item):
         if action == "ignore":
             continue
 
-        if "DeprecationWarning" not in warning._category_name:
-            continue
-
         warning.item = item
         quadruplet = (warning.filename, warning.lineno, warning.category, str(warning.message))
 
@@ -109,6 +103,7 @@ def pytest_runtest_call(item):
 
 @pytest.hookimpl(hookwrapper=True)
 def pytest_terminal_summary(terminalreporter, exitstatus, config=None):
+    
     pwd = os.path.realpath(os.curdir)
 
     def cut_path(path):
@@ -117,36 +112,27 @@ def pytest_terminal_summary(terminalreporter, exitstatus, config=None):
         if "/site-packages/" in path:  # tox install the package in general
             path = path.split("/site-packages/")[1]
         return path
-
+        
     def format_test_function_location(item):
         return "%s::%s:%s" % (item.location[0], item.location[2], item.location[1])
 
     yield
-
-    # try to grab tox env name because tox don't give it
-    for test in terminalreporter.stats.get("passed", []) + terminalreporter.stats.get("failed", []):
-        if ".tox/" in test.location[0]:
-            tox_env_name = test.location[0].split(".tox/")[1].split("/")[0]
-            output_file_name = "%s-deprecated-warnings.json" % tox_env_name
-            break
-    else:
-        for warning in terminalreporter.stats.get("warnings", []):
-            if ".tox/" in warning.fslocation[0]:
-                tox_env_name = warning.fslocation[0].split(".tox/")[1].split("/")[0]
-                output_file_name = "%s-deprecated-warnings.json" % tox_env_name
-                break
+    
+    def output_file():
+        if os.path.exists("/workspace"):
+            return '/workspace/warnings.txt'
         else:
-            output_file_name = "deprecated-warnings.json"
+            return 'warnings.txt'
 
     if counted_warnings:
         print("")
-        print("Deprecated warnings summary:")
+        print("warnings summary:")
         print("============================")
         for warning in sorted(counted_warnings.values(), key=lambda x: (x.filename, x.lineno)):
             print("%s\n-> %s:%s %s('%s')" % (format_test_function_location(warning.item), cut_path(warning.filename), warning.lineno, warning.category.__name__, warning.message))
 
         print("")
-        print("All DeprecationWarning errors can be found in the %s file." % output_file_name)
+        print(f"All Warning errors can be found in the {output_file()} file.")
 
         warnings_as_json = []
 
@@ -173,32 +159,28 @@ def pytest_terminal_summary(terminalreporter, exitstatus, config=None):
                 else:
                     serialized_frame["file_content"] = None
                 serialized_frame["filename"] = cut_path(serialized_frame["filename"])
+                # d[serialized_frame.encode("ascii", "ignore").decode('utf-8')
                 serialized_traceback.append(serialized_frame)
 
             serialized_warning.update({
-                "count": warning.count,
+                "path": cut_path(warning.filename),
                 "lineno": warning.lineno,
-                "category": warning.category.__name__,
-                "path": warning.filename,
-                "filename": cut_path(warning.filename),
-                "test_file": warning.item.location[0],
-                "test_lineno": warning.item.location[1],
-                "test_name": warning.item.location[2],
-                "file_content": open(warning.filename, "r").read(),
-                "dependencies": {x.metadata["Name"].lower(): x.metadata["Version"] for x in importlib_metadata.distributions()},
-                "formatted_traceback": "".join(warning.formatted_traceback),
-                "traceback": serialized_traceback,
+                "count":1,
+                "warning_message": str(warning.message),
             })
 
-            if "with_traceback" in serialized_warning:
-                del serialized_warning["with_traceback"]
+            # How we format the warnings
+            #<filename/path>:<line number>:<character number> <warning message>
 
+            if "with_traceback" in serialized_warning:
+                del serialized_warning["with_traceback"]            
             warnings_as_json.append(serialized_warning)
 
-        with open(output_file_name, "w") as f:
-            f.write(json.dumps(warnings_as_json, indent=4, sort_keys=True))
-            f.write("\n")
+        with open(output_file(), "w") as f:
+            for i in warnings_as_json: 
+                f.write( f'{i["path"]}:{i["lineno"]}:1:{i["warning_message"]}')
+                f.write('\n')
     else:
         # nothing, clear file
-        with open(output_file_name, "w") as f:
+        with open(output_file(), "w") as f:
             f.write("")
